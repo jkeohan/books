dceb565a7930f81fd2ed39a4d283f2782aa35f2c
jkeohan <jkeohan@gmail.com>
2014-12-18 12:03:29 -0500

    server.js updated

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 var index = "index.html";
 var fs = require("fs");
 var http = require("http");
-var server = http.createServer(function(req, res){
+var server = http.createServer(function(req,res){
   var path = req.url;
   var title = path.split("/");
   fs.readFile("index.html", function(err, data){
     var htmlText = data.toString();
-    console.log(htmlText);
-    fs.readFile("books.txt"), function(err, data){
-      var bookText = data.toString();
-      var htmlText = htmlText.replace("<replace>", bookText);
+    //console.log(htmlText);
+    fs.readFile("books.txt", function(err, data){
+      //console.log("Inside seconde readFile");
+      //console.log(htmlText);
+
+      var bookText = data.toString().split("\n")
+
+      // bookText.forEach( function (val){
+     
+      // })
+      var bookText2 = [];
+      
+      bookText.forEach(function(x) {
+        bookText2.push(x)
+        bookText2.push("<br>")
+      });
+
+
+      console.log(bookText2.join("\n"));
+      htmlText = htmlText.replace("<REPLACEME>", bookText2.join("\n"))
+    
       fs.writeFile("index.html", htmlText, function(err){});
-      server.res(index.html);
-    }
+      res.end(htmlText);
+    
+      });
+      //console.log(htmlText);
+
+
   });
 });
+
 server.listen(2000);
+
+/*<html>
+  <body>
+    <p>
+     
+    </p>
+  </body>
+</html>*/
